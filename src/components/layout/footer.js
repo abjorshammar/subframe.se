b01d4831ed97685d81e22fa3cdd85d3971ff4353
Andreas Björshammar <andreas@bjorshammar.se>
2017-06-15 16:20:17 +0200

    Added text to footer

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -4,25 +4,26 @@ export default class Footer extends React.Component {
   render() {
     return (
         <footer className="page-footer grey darken-3">
-          {/* Add more information
           <div className="container">
             <div className="row">
               <div className="col l6 s12">
-                <h5 className="white-text">Footer Content</h5>
-                <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 className="grey-text text-lighten-1">Subframe AB</h5>
+                <p className="grey-text">Subframe grundades med huvudsyftet att sälja IT-konsulttjänster med fokus på infrastruktur, devops samt enklare webutveckling.
+                  Vi jobbar också med video i form av streaming eller mindre videoproduktioner, samt olika former av 3D CAD och 3D printing.</p>
               </div>
               <div className="col l4 offset-l2 s12">
-                <h5 className="white-text">Links</h5>
+                <h6 className="grey-text text-lighten-1">Stack</h6>
                 <ul>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                  <li><a className="grey-text" href="https://facebook.github.io/react/">React</a></li>
+                  <li><a className="grey-text" href="http://materializecss.com">Materialize CSS</a></li>
+                  <li><a className="grey-text" href="https://www.docker.com/">Docker</a></li>
+                  <li><a className="grey-text" href="https://aws.amazon.com">AWS S3 & CF</a></li>
+                  <li><a className="grey-text" href="https://github.com/abjorshammar/subframe.se">Github</a></li>
+                  <li><a className="grey-text" href="https://codeship.com/">Codeship</a></li>
                 </ul>
               </div>
             </div>
           </div>
-          */}
           <div className="footer-copyright grey-text text-darken">
             <div className="container center-align">
             © Subframe AB
